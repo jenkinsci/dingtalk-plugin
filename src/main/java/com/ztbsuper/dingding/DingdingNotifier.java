18e43fe0bbb10b8bfb266eb1f70382cfb4049adb
程竹江 <chengzj@pingpongx.com>
2019-06-28 17:57:17 +0800

    Provider getter of onAbort property since it would be used in the page

--- src/main/java/com/ztbsuper/dingding/DingdingNotifier.java
+++ src/main/java/com/ztbsuper/dingding/DingdingNotifier.java
@@ -57,8 +57,8 @@ public class DingdingNotifier extends Notifier {
     public boolean isOnFailed() {
         return onFailed;
     }
-    
-    public boolean onAbort() {
+
+    public boolean isOnAbort() {
         return onAbort;
     }
 
