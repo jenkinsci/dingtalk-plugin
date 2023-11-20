db6cb22f07999448cc4839375598677d82ae420e
liuwei <wei2460@qq.com>
2023-11-20 12:17:59 +0800

    fix: jenkins 移除 prototype 导致机器人配置测试功能报错 close #223

--- src/main/webapp/scripts/robotConfigValidator.js
+++ src/main/webapp/scripts/robotConfigValidator.js
@@ -1,117 +1,95 @@
 (function ($) {
-  if (!$) {
-    throw new Error('jQuery 插件加载失败将无法校验机器人配置，但不影响正常使用')
-  }
+	if (!$) {
+		throw new Error('jQuery 插件加载失败将无法校验机器人配置，但不影响正常使用')
+	}
 
-  $(function () {
-    $(document).on('click', '.robot-config-validate-btn', validateRobotConfig)
-  })
+	$(function () {
+		$(document).on('click', '.robot-config-validate-btn', validateRobotConfig)
+	})
 
-  function applyErrorMessage(elt, rsp) {
-    if (rsp.status == 200) {
-      elt.innerHTML = rsp.responseText;
-    } else {
-      var id = 'valerr' + (iota++);
-      elt.innerHTML = '<a href="" onclick="document.getElementById(\'' + id
-          + '\').style.display=\'block\';return false">ERROR</a><div id="'
-          + id + '" style="display:none">' + rsp.responseText + '</div>';
-      var error = document.getElementById('error-description'); // cf. oops.jelly
-      if (error) {
-        var div = document.getElementById(id);
-        while (div.firstElementChild) {
-          div.removeChild(div.firstElementChild);
-        }
-        div.appendChild(error);
-      }
-    }
-    Behaviour.applySubtree(elt);
-  }
+	function applyErrorMessage(elt, rsp) {
+		if (rsp.status == 200) {
+			elt.innerHTML = rsp.responseText
+		} else {
+			var id = 'valerr' + (iota++)
+			elt.innerHTML = '<a href="" onclick="document.getElementById(\'' + id
+					+ '\').style.display=\'block\';return false">ERROR</a><div id="'
+					+ id + '" style="display:none">' + rsp.responseText + '</div>'
+			var error = document.getElementById('error-description') // cf. oops.jelly
+			if (error) {
+				var div = document.getElementById(id)
+				while (div.firstElementChild) {
+					div.removeChild(div.firstElementChild)
+				}
+				div.appendChild(error)
+			}
+		}
+		Behaviour.applySubtree(elt)
+	}
 
-  function validateRobotConfig() {
-    var $btn = $(this)
+	async function validateRobotConfig() {
+		var $btn = $(this)
 
-    var checkUrl = $btn.attr('data-validate-button-descriptor-url') +
-        "/" +
-        $btn.attr('data-validate-button-method')
+		var checkUrl = $btn.attr('data-validate-button-descriptor-url') +
+				'/' +
+				$btn.attr('data-validate-button-method')
 
-    var $robot = $btn.parents('.robot-config-container')
-    var $msg = $robot.find('.robot-config-validate-msg')
+		var $robot = $btn.parents('.robot-config-container')
+		var $msg = $robot.find('.robot-config-validate-msg')
 
-    var startRequest = function () {
-      $msg.empty()
-    }
+		$msg.empty()
 
-    var completeRequest = function (res) {
-      applyErrorMessage($msg.get(0), res)
-      layoutUpdateCallback.call()
+		var url = new URL(checkUrl, window.location.origin)
+		getParameters($robot).forEach(function (v, k) {
+			url.searchParams.set(k, v)
+		})
 
-      var s = res.getResponseHeader("script")
+		var res = await fetch(url, {
+			method: 'GET'
+		})
 
-      try {
-        eval(s)
-      } catch (e) {
-        applyErrorMessage(
-            $msg.get(0),
-            "failed to evaluate " +
-            s +
-            "\n" +
-            e.message
-        )
-      }
-    }
+		var resText = await res.text()
+		applyErrorMessage($msg.get(0), {
+			status: res.status,
+			responseText: resText
+		})
+	}
 
-    startRequest()
+	function getParameters($robot) {
+		/**
+		 * 代理信息
+		 */
+		var $proxy = $('#proxyConfigContainer')
+		var proxyConfig = {
+			type: $proxy.find('select[name="type"]').val(),
+			host: $proxy.find('input[name="host"]').val(),
+			port: $proxy.find('input[name="port"]').val()
+		}
 
-    new Ajax.Request(checkUrl, {
-      parameters: getParameters($robot),
-      onComplete: function (res) {
-        setTimeout(function () {
-          completeRequest(res)
-        }, 300)
-      }
-    })
-  }
+		/**
+		 * 机器人配置
+		 */
+		var id = $robot.find('input[name="id"]').val()
+		var name = $robot.find('input[name="name"]').val()
+		var webhook = $robot.find('input[name="webhook"]').val()
+		// 安全策略
+		var securityPolicyConfigs = $.map(
+				$robot.find('.security-config-container'),
+				function (t) {
+					return {
+						type: $(t).find('input[name=type]').val(),
+						value: $(t).find('input[name=value]').val()
+					}
+				})
 
-  function getParameters($robot) {
-    /**
-     * 代理信息
-     */
-    var $proxy = $('#proxyConfigContainer')
-    var proxyConfig = {
-      type: $proxy.find('select[name="type"]').val(),
-      host: $proxy.find('input[name="host"]').val(),
-      port: $proxy.find('input[name="port"]').val()
-    }
+		var params = new URLSearchParams()
 
-    /**
-     * 机器人配置
-     */
-    var id = $robot.find('input[name="id"]').val()
-    var name = $robot.find('input[name="name"]').val()
-    var webhook = $robot.find('input[name="webhook"]').val()
-    // 安全策略
-    var securityPolicyConfigs = $.map(
-        $robot.find('.security-config-container'),
-        function (t) {
-          return {
-            type: $(t).find('input[name=type]').val(),
-            value: $(t).find('input[name=value]').val()
-          }
-        })
+		params.set('id', id)
+		params.set('name', name)
+		params.set('webhook', webhook)
+		params.set('securityPolicyConfigs', JSON.stringify(securityPolicyConfigs))
+		params.set('proxy', JSON.stringify(proxyConfig))
 
-    var toJSON = Array.prototype.toJSON
-    Array.prototype.toJSON = undefined
-
-    var result = {
-      id: id,
-      name: name,
-      webhook: webhook,
-      securityPolicyConfigs: JSON.stringify(securityPolicyConfigs),
-      proxy: JSON.stringify(proxyConfig)
-    }
-
-    Array.prototype.toJSON = toJSON
-
-    return result
-  }
+		return params
+	}
 })(jQuery3 || jQuery)
