1b9128be2c5f11aa823ea85b07bf8336c62c8f4f
BobDu <i@bobdu.cc>
2024-01-26 18:26:39 +0800

    fix no toggle custom context/message textarea
    Signed-off-by: BobDu <i@bobdu.cc>

--- src/main/webapp/scripts/notifierConfig.js
+++ src/main/webapp/scripts/notifierConfig.js
@@ -1,16 +1,11 @@
-window.addEventListener('load', function () {
-	document.querySelector(
-			'.dt-notifier-config-raw input[name="_.raw"]'
-	).addEventListener('change',
-			function (event) {
-		console.log(document.querySelector('.raw-content'))
-				if (event.target.checked) {
-					document.querySelector('.dt-raw-content-builtin').style.display = 'none'
-					document.querySelector('.dt-raw-content-custom').style.display = ''
-				} else {
-					document.querySelector('.dt-raw-content-builtin').style.display = ''
-					document.querySelector('.dt-raw-content-custom').style.display = 'none'
-				}
-			})
+Behaviour.specify('.dt-notifier-config-raw', 'dt-notifier-config-raw', 0, function (element) {
+    element.onchange = function () {
+        if (element.checked) {
+            document.querySelector('.dt-raw-content-builtin').style.display = 'none'
+            document.querySelector('.dt-raw-content-custom').style.display = ''
+        } else {
+            document.querySelector('.dt-raw-content-builtin').style.display = ''
+            document.querySelector('.dt-raw-content-custom').style.display = 'none'
+        }
+    }
 })
-
