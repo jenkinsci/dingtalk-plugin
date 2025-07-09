de5908a5ca71a1b25c4039e55706f66fea0c3694
Bob Du <i@bobdu.cc>
2025-07-09 15:35:52 +0800

    remove custom http sdk
    Signed-off-by: Bob Du <i@bobdu.cc>

--- src/main/java/io/jenkins/plugins/model/RobotConfigModel.java
+++ src/main/java/io/jenkins/plugins/model/RobotConfigModel.java
@@ -6,18 +6,17 @@ import io.jenkins.plugins.enums.SecurityPolicyEnum;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
+
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.HmacAlgorithms;
+import org.apache.commons.codec.digest.HmacUtils;
 import org.apache.commons.lang3.StringUtils;
 
 /**
- * 机器人配置信息
+ * Robot configuration information
  *
  * @author liuwei
  */
@@ -25,75 +24,72 @@ import org.apache.commons.lang3.StringUtils;
 @Slf4j
 public class RobotConfigModel {
 
-  /** 关键字 */
-  private String keys;
+    /**
+     * Keywords
+     */
+    private String keys;
 
-  /** 签名 */
-  private String sign;
+    /**
+     * Signature
+     */
+    private String sign;
 
-  /** api 接口 */
-  private String webhook;
+    /**
+     * API endpoint
+     */
+    private String webhook;
 
-  public String getServer() {
-    if (StringUtils.isEmpty(sign)) {
-      return webhook;
+    public String getServer() {
+        if (StringUtils.isEmpty(sign)) {
+            return webhook;
+        }
+        long timestamp = System.currentTimeMillis();
+        return webhook + "&timestamp=" + timestamp + "&sign=" + createSign(timestamp, sign);
     }
-    long timestamp = System.currentTimeMillis();
-    return webhook + "&timestamp=" + timestamp + "&sign=" + createSign(timestamp, sign);
-  }
 
-  /**
-   * 从机器人配置中解析元信息
-   *
-   * @param robotConfig 配置
-   * @return 机器人配置
-   */
-  public static RobotConfigModel of(DingTalkRobotConfig robotConfig) {
-    ArrayList<DingTalkSecurityPolicyConfig> securityPolicyConfigs =
-        robotConfig.getSecurityPolicyConfigs();
-    RobotConfigModel meta = new RobotConfigModel();
-    meta.setWebhook(robotConfig.getWebhook());
-    // 解析安全策略
-    securityPolicyConfigs.forEach(
-        config -> {
-          if (StringUtils.isEmpty(config.getValue())) {
-            return;
-          }
-          String type = config.getType();
-          SecurityPolicyEnum securityPolicyEnum = SecurityPolicyEnum.valueOf(type);
-          switch (securityPolicyEnum) {
-            case KEY:
-              meta.setKeys(config.getValue());
-              break;
-            case SECRET:
-              meta.setSign(config.getValue());
-              break;
-            default:
-	            log.error("对应的安全策略不存在：{}", type);
-          }
-        });
-    return meta;
-  }
-
-  /**
-   * 签名方法
-   *
-   * @return 签名
-   */
-  private static String createSign(long timestamp, String secret) {
-    String result = "";
-    try {
-      String seed = timestamp + "\n" + secret;
-      Mac mac = Mac.getInstance("HmacSHA256");
-      mac.init(new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256"));
-      byte[] signData = mac.doFinal(seed.getBytes(StandardCharsets.UTF_8));
-      result =
-          URLEncoder.encode(
-              new String(Base64.encodeBase64(signData), StandardCharsets.UTF_8),
-		          StandardCharsets.UTF_8);
-    } catch (NoSuchAlgorithmException | InvalidKeyException e) {
-      log.error("钉钉插件设置签名失败：", e);
+    /**
+     * Parse metadata from robot configuration
+     *
+     * @param robotConfig configuration
+     * @return robot configuration
+     */
+    public static RobotConfigModel of(DingTalkRobotConfig robotConfig) {
+        ArrayList<DingTalkSecurityPolicyConfig> securityPolicyConfigs =
+                robotConfig.getSecurityPolicyConfigs();
+        RobotConfigModel meta = new RobotConfigModel();
+        meta.setWebhook(robotConfig.getWebhook());
+        // Parse security policies
+        securityPolicyConfigs.forEach(
+                config -> {
+                    if (StringUtils.isEmpty(config.getValue())) {
+                        return;
+                    }
+                    String type = config.getType();
+                    SecurityPolicyEnum securityPolicyEnum = SecurityPolicyEnum.valueOf(type);
+                    switch (securityPolicyEnum) {
+                        case KEY:
+                            meta.setKeys(config.getValue());
+                            break;
+                        case SECRET:
+                            meta.setSign(config.getValue());
+                            break;
+                        default:
+                            log.error("Corresponding security policy does not exist: {}", type);
+                    }
+                });
+        return meta;
     }
-    return result;
-  }
+
+    /**
+     * Signature method
+     * <a href="https://open.dingtalk.com/document/orgapp/customize-robot-security-settings#title-7fs-kgs-36x">...</a>
+     *
+     * @return signature
+     */
+    static String createSign(long timestamp, String secret) {
+        String stringToSign = timestamp + "\n" + secret;
+
+        byte[] hmac = new HmacUtils(HmacAlgorithms.HMAC_SHA_256, secret).hmac(stringToSign);
+        return URLEncoder.encode(Base64.encodeBase64String(hmac), StandardCharsets.UTF_8);
+    }
 }
